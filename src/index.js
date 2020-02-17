a6d99fa9413636f1d376fc446e3e21f74cd87028
Paulo Filho <pcf.feat@gmail.com>
2020-02-17 07:15:54 -0400

    feat: add prisma approach to a graphql-empowered server, using prisma cloud as storage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
+const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 
-async function main() {
-  // Create a new link
-  const newLink = await prisma.createLink({
-    url: "www.prisma.io",
-    description: "Prisma replaces traditional ORMs"
-  });
-  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
+// resolvers that map query to data retrieval
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    }
+  },
+  Mutation: {
+    post: (root, { url, description }, context) => {
+      return context.prisma.createLink({
+        url,
+        description
+      });
+    }
+  }
+};
 
-  // Read all links from the database and print them to the console
-  const allLinks = await prisma.links();
-  console.log(allLinks);
-}
+// start up the server passing previously set schema/resolvers
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: { prisma }
+});
 
-main().catch(e => console.error(e));
+server.start(() => console.log(`Server is running on http://localhost:4000`));
