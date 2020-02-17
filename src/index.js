9ae09d703a2e3b598349f58302dd9c1d3ba6d85e
Paulo Filho <pcf.feat@gmail.com>
2020-02-17 09:11:08 -0400

    feat: add subscriptions to data changes via Prisma API, allow for links to be upvoted and monitored

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Vote = require("./resolvers/Vote");
 const Subscription = require("./resolvers/Subscription");
 
 // resolvers that map query to data retrieval
@@ -13,6 +14,7 @@ const resolvers = {
   Mutation,
   Subscription,
   User,
+  Vote,
   Link
 };
 
