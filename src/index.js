680ba467d1b71b072485afca1dba68fd9242327c
Paulo Filho <pcf.feat@gmail.com>
2020-02-17 08:33:56 -0400

    refactor: split resolvers into separate files and handle authorized endpoints with a centralized handler

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 // resolvers that map query to data retrieval
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.prisma.links();
-    }
-  },
-  Mutation: {
-    post: (root, { url, description }, context) => {
-      return context.prisma.createLink({
-        url,
-        description
-      });
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 // start up the server passing previously set schema/resolvers
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
-  context: { prisma }
+  context: request => {
+    return {
+      ...request,
+      prisma
+    };
+  }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
