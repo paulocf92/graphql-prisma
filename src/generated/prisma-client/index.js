01e723a0dc296a8af2b51fb6a7ca17c560f33ca2
Paulo Filho <pcf.feat@gmail.com>
2020-02-17 05:47:33 -0400

    chore: initial server structure with Prisma

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/paulo-filho-ee2d2f/graphql-prisma/dev`
+});
+exports.prisma = new exports.Prisma();
